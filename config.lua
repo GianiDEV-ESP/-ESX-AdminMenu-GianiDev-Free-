45fb515a2d9dd235a90086b9da62f9648e50f645
GianiDEV-ESP <gianifiert@gmail.com>
2025-04-11 12:09:19 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,5 +2,5 @@ Config = {}
 
 -- Identificadores que pueden abrir el menú (license)
 Config.AdminList = {
-    "license:a4f1e1e4813ee34f261e4651e89011883fdc09e3",
+    "license:xxxxxxxxxxxxxxxxxxxxxxxxx",
 }
